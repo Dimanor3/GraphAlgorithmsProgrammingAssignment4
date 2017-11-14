87ebaa66b83b20d20edf69a25cab0700688d73e5
ethoma53 <ethoma53@uncc.edu>
2017-11-13 19:32:55 -0500

    Updated FW

--- FW.java
+++ FW.java
@@ -1,80 +1,97 @@
 import static java.lang.String.format;
 import java.util.Arrays;
+import java.util.List;
  
 public class FloydWarshall {
 	
-	private int[][] edgeWeights;
-	private String shortestPath;
-	private double[][] fwMatrix;
-	private int[][] nextEdge;
-	private final double POSITIVE_INF = double.POSITIVE_INFINITY;
+	private List<vertex> vertices;
+	private List<vertex> edges;
+	private int numVertices;
+	private int[][] fwMatrix;
+	private int[][] nextMatrix;
+	private String shortestPaths;
+	private final int POSITIVE_INF = (int)double.POSITIVE_INFINITY;
 	
 	public FW(Graph G) 
 	{
-		int[][] edgeWeights = new int[G.numEdges()][3];
+		vertices = G.getVerticesG();
 		edges = G.getEdgiesG();
-		i, j = 0;
-		for (edge e: edgies)
+		numV = vertices.size();
+		
+		fwMatrix = new int[numV][numV];
+		for (int i = 0; i < numV; i++)
 		{
-			edgeWeights[i][j] = e.getBeginLocation();
-			edgeWeights[i][j + 1] = e.getEndLocation();
-			edgeWeights[i][j + 2] = e.getWeightofEdge();
-			i++;
+			for (int j = 0; j < numV; j++)
+			{
+				fwMatrix[i][j] = POSITIVE_INF;
+				nextMatrix[i][j] = 0;
+			}
 		}
 		
-		fwMatrix = new double[G.numVertices()][G.numVertices()];
-        for (double[] eachRow : fwMatrix)
-            Arrays.fill(eachRow, POSITIVE_INF);
- 
-        for (int[] weight : edgeWeights)
-            fwMatrix[weight[0] - 1][weight[1] - 1] = weight[2];
- 
-        nextEdge = new int[G.numVertices()][G.numVertices()];
-        for (int k = 0; k < nextEdge.length; k++) {
-            for (int h = 0; h < nextEdge.length; h++)
-                if (k != h)
-                    nextEdge[k][h] = h + 1;
-        }
+		for (edge eachEdge: edges)
+		{
+			fwMatrix[vertices.indexOf(eachEdge.getBeginLocation())][vertices.indexOf(eachEdge.getEndLocation())] 
+			= eachEdge.getWeightOfEdge();
+		}
 	}
  
     public void doFW() {
-        for (int x = 0; x < numVertices; x++){
-            for (int y = 0; y < numVertices; y++){
-                for (int z = 0; z < numVertices; z++){
-                    if (fwMatrix[y][x] + fwMatrix[x][z] < fwMatrix[y][z]) {
-                        fwMatrix[y][z] = fwMatrix[y][x] + fwMatrix[x][z];
-                        nextEdge[y][z] = nextEdge[y][x];
+        for (int x = 0; x < numV; x++){
+            for (int y = 0; y < numV; y++){
+                for (int z = 0; z < numV; z++){
+                    if (y == z)
+					{
+						fwMatrix[y][z] = 0;
                     }
+					else
+					{
+						int yz = fwMatrix[y][z];
+                        int yx = fwMatrix[y][x];
+                        int xz = fwMatrix[x][z];
+                        int sum = (yx != POSITIVE_INF && xz != POSITIVE_INF) ? (yx + xz): Integer.MAX_VALUE;
+                        if (yz > sum)
+                            sums[y][z] = sum;
+							nextMatrix[y][z] = x;
+					}
 				}
 			}
 		}
-        shortestPath = getResult(fwMatrix, nextEdge);
+        shortestPaths = getResult();
     }
 	
 	public String getSPairsPath()
 	{
-		return shortestPath;
+		return shortestPaths;
 	}
 	
+	public List<vertex> getPath(int i, int j)
+	{
+		List<vertex> path = new List();
+		
+		getPath(i, P[i][j]);
+		path += vertices.get(P[i][j]);
+		getPath(P[i][j], j);
+		return path;
+	}
  
-    private static String getResult(double[][] fwMatrix, int[][] nextEdge) {
-        StringBuilder SB = new StringBuilder();
-		SB.append("vertices   path weight   shortest path \n");
-        for (int k = 0; k < nextEdge.length; k++) {
-            for (int h = 0; h < nextEdge.length; h++) {
-                if (k != h) {
-                    int a = k + 1;
-                    int b = h + 1;
-                    String resultString = format("%d --> %d    %2d     %s", a, b,
-                            (int)fwMatrix[k][h], a);
-                    do {
-                        u = nextEdge[a - 1][b - 1];
-                        resultString += " --> " + a;
-                    } while (a != b);
-                    SB.append(resultString + "\n");
-                }
-            }
-        }
+    private String getResult() 
+	{   
+		List<vertex> tempPath;
+		StringBuilder SB = new StringBuilder();
+		for (int k = 0; k < numV; k++) {
+            for (int h = 0; h < numV; h++) {
+                if (fwMatrix[k][h] != POSITIVE_INF) {
+					SB.append("\nShortest Path from: " + vertices.get(k).getDolphinName() + " to: " 
+					+ vertices.get(h).getDolphinName() + "\n");
+					tempPath = getPath(k, h);
+					for (vertex v: tempPath)
+					{	
+						SB.append(v.getDolphinName() + " --> ");
+					}
+					SB.append("\nTotal Path Weight: " + fwMatrix[h][k] + " ");
+				}
+			}
+		}
 		return SB.toString();
     }
 }
